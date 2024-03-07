df7d200dfe782ba9574769d886ff415cef58d490
goraebap <dev.goraebap@gmail.com>
2024-03-07 17:16:36 +0900

    setting: tailwindcss 디자인 시스템 적용, 타이틀 폰트 추가 및 테스트

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,28 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: {
+      tablet: '960px',
+      desktop: '1248px'
+    },
+    fontSize: {
+      xs: ['14px', { lineHeight: '24px', letterSpacing: '-0.03em' }],
+      sm: ['16px', { lineHeight: '28px', letterSpacing: '-0.03em' }],
+      lg: ['18px', { lineHeight: '28px', letterSpacing: '-0.03em' }],
+      xl: ['24px', { lineHeight: '36px', letterSpacing: '-0.03em' }],
+      '2xl': ['36px', { lineHeight: '48px', letterSpacing: '-0.032em' }],
+      '3xl': ['48px', { lineHeight: '56px', letterSpacing: '-0.032em' }],
+      '4xl': ['56px', { lineHeight: '64px', letterSpacing: '-0.032em' }],
+      '5xl': ['80px', { lineHeight: '80px', letterSpacing: '-0.032em' }],
+    },
+    boxShadow: {
+      sm: '0px 2px 4px 0px rgba(11, 10, 55, 0.15',
+      lg: '0px 8px 20px 0px rgba(18, 16, 99, 0.06',
+    },
+    fontFamily: {
+      pretendard: 'Pretendard Variable, sans-serif',
+      origa: 'origa, sans-serif',
+    },
     extend: {},
   },
   plugins: [],
