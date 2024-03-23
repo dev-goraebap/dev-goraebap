0af3444a3f28115321a49704e8a0e2cf696052fb
goraebap <dev.goraebap@gmail.com>
2024-03-23 19:21:02 +0900

    feat: 거리에 따라 오브젝트 상호작용 기능 추가

--- src/shared/phaser/scenes/test.scene.ts
+++ src/shared/phaser/scenes/test.scene.ts
@@ -1,12 +1,18 @@
 import * as Phaser from 'phaser';
 
+import { BookTableAnims, ComputerAnims, GameMachineAnims, PianoAnims } from '../constants';
 import { BirdController } from '../controllers';
 import { BirdPrefab, BookTablePrefab, ComputerPrefab, FireplacePrefab, GameMachinePrefab, PianoPrefab } from '../prefabs';
-import { ComputerAnims } from '../constants';
 
 export class TestScene extends Phaser.Scene {
 
     private player!: BirdPrefab;
+    private computer!: ComputerPrefab;
+    private bookTable!: BookTablePrefab;
+    private fireplace!: FireplacePrefab;
+    private piano!: PianoPrefab;
+    private gameMachine!: GameMachinePrefab;
+
     private inputHandler!: BirdController;
 
     constructor() {
@@ -36,30 +42,23 @@ export class TestScene extends Phaser.Scene {
         this.cameras.main.scrollY = (mapHeight - screenHeight) / 2;
         this.cameras.main.setZoom(3);
 
-        // 타일맵 오브젝트 설정
-        let computer!: Phaser.Physics.Arcade.Sprite;
-        let bookTable!: Phaser.Physics.Arcade.Sprite;
-        let gameMachine!: Phaser.Physics.Arcade.Sprite;
-        let piano!: Phaser.Physics.Arcade.Sprite;
-        let fireplace!: Phaser.GameObjects.Sprite;
-
         const objectsLayer = tilemap.getObjectLayer("objects")!.objects as any[];
         objectsLayer.forEach(obj => {
             switch (obj.name) {
                 case "computer":
-                    computer = new ComputerPrefab(this, obj.x, obj.y);
+                    this.computer = new ComputerPrefab(this, obj.x, obj.y);
                     break;
                 case "bookTable":
-                    bookTable = new BookTablePrefab(this, obj.x, obj.y);
+                    this.bookTable = new BookTablePrefab(this, obj.x, obj.y);
                     break;
                 case "gameMachine":
-                    gameMachine = new GameMachinePrefab(this, obj.x, obj.y);
+                    this.gameMachine = new GameMachinePrefab(this, obj.x, obj.y);
                     break;
                 case "piano":
-                    piano = new PianoPrefab(this, obj.x, obj.y);
+                    this.piano = new PianoPrefab(this, obj.x, obj.y);
                     break;
                 case "fireplace":
-                    fireplace = new FireplacePrefab(this, obj.x, obj.y);
+                    this.fireplace = new FireplacePrefab(this, obj.x, obj.y);
                     break;
                 case "spawn":
                     this.player = new BirdPrefab(this, obj.x, obj.y);
@@ -79,18 +78,68 @@ export class TestScene extends Phaser.Scene {
             arcadeObj.setVisible(false); // 충돌 오브젝트들은 타일맵이 아니라 도형을 이용해서 만듬 -> 보여줄 필요 없음
         });
 
-        this.physics.add.collider(this.player, computer, (a: any, b: any) => {
-            console.log(a);
-            console.log(b);
-            b.anims.play(ComputerAnims.Select);
-        });
-        this.physics.add.collider(this.player, bookTable);
-        this.physics.add.collider(this.player, gameMachine);
-        this.physics.add.collider(this.player, piano);
+        this.physics.add.collider(this.player, this.computer);
+        this.physics.add.collider(this.player, this.bookTable);
+        this.physics.add.collider(this.player, this.gameMachine);
+        this.physics.add.collider(this.player, this.piano);
         this.physics.add.collider(this.player, collideObjects);
     }
 
     override update(time: number, delta: number): void {
         this.inputHandler.update();
+
+        if (!this.player || !this.computer || !this.bookTable ||!this.gameMachine) {
+            return;
+        }
+
+        // 모든 객체에 대해 거리를 계산하고 가장 가까운 객체를 찾습니다.
+        let closestObject: any = null;
+        let smallestDistance = Infinity;
+        const objects = [this.computer, this.bookTable, this.gameMachine, this.piano]; // 검사할 객체 배열
+
+        objects.forEach(object => {
+            const distance = Phaser.Math.Distance.Between(
+                this.player.x, this.player.y,
+                object.x, object.y
+            );
+
+            if (distance < smallestDistance) {
+                smallestDistance = distance;
+                closestObject = object;
+            }
+        });
+
+        // 가장 가까운 객체와의 거리가 일정 범위 내에 있는지 확인하고,
+        // 해당 객체에만 선택 애니메이션을 적용합니다.
+        objects.forEach(object => {
+            if (object === closestObject && this.checkOverlap(this.player, object)) {
+                if (object === this.computer) {
+                    this.computer.anims.play(ComputerAnims.Select);
+                } else if (object === this.bookTable) {
+                    this.bookTable.anims.play(BookTableAnims.Select);
+                } else if (object === this.gameMachine) {
+                    this.gameMachine.anims.play(GameMachineAnims.Select);
+                } else if (object === this.piano) {
+                    this.piano.anims.play(PianoAnims.Select);
+                }
+            } else {
+                // 선택되지 않은 객체는 일반 상태로 유지
+                if (object === this.computer) {
+                    this.computer.anims.play(ComputerAnims.Idle);
+                } else if (object === this.bookTable) {
+                    this.bookTable.anims.play(BookTableAnims.Idle);
+                } else if (object === this.gameMachine) {
+                    this.gameMachine.anims.play(GameMachineAnims.Idle);
+                } else if (object === this.piano) {
+                    this.piano.anims.play(PianoAnims.Idle);
+                }
+            }
+        });
+    }
+
+    private checkOverlap(spriteA: Phaser.Physics.Arcade.Sprite, spriteB: Phaser.Physics.Arcade.Sprite) {
+        const boundsA = spriteA.getBounds();
+        const boundsB = spriteB.getBounds();
+        return Phaser.Geom.Intersects.RectangleToRectangle(boundsA, boundsB);
     }
 }
