59c58d76a42c551887ca8e6dfa0a35226bcf186c
dev-goraebap <dev.goraebap@gmail.com>
2025-07-31 15:09:04 +0900

    chore: update eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import prettierConfig from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'resources/**/*.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  prettierConfig,
   {
     languageOptions: {
       globals: {
