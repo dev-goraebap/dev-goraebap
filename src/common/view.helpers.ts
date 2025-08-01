554da2077354cb1d0b5e3e288f806f6968d17423
dev-goraebap <dev.goraebap@gmail.com>
2025-08-01 15:06:49 +0900

    feat: add currentThemeHelper

---
+++ src/common/view.helpers.ts
@@ -0,0 +1,44 @@
+import { Request } from 'express';
+import { ViewHelperFactory } from 'nestjs-mvc-tools';
+
+/**
+ * 현재 라우트인지 확인하는 헬퍼
+ * 템플릿에서 {{ isCurrentRoute('/home') }} 형태로 사용
+ */
+export const isCurrentRouteHelper: ViewHelperFactory = (req: Request) => {
+  return {
+    key: 'isCurrentRoute',
+    fn: (routePath: string, exact: boolean = false) => {
+      if (!exact) {
+        return (
+          req.originalUrl.startsWith(routePath) ||
+          req.path.startsWith(routePath)
+        );
+      }
+      return req.originalUrl === routePath || req.path === routePath;
+    },
+  };
+};
+
+/**
+ * 현재 태마를 제공하는 헬퍼
+ * 템플릿에서 {{ currentTheme() }} 형태로 사용
+ */
+export const currentThemeHelper: ViewHelperFactory = (req: Request) => {
+  return {
+    key: 'currentTheme',
+    fn: () => {
+      const defaultTheme = 'light';
+      const cookies = req.cookies as Record<string, string>;
+      if (!cookies) {
+        console.debug('쿠키가 활성화되지 않았습니다.');
+        return defaultTheme;
+      }
+      const theme = cookies?.theme;
+      if (!theme) {
+        return defaultTheme;
+      }
+      return theme;
+    },
+  };
+};
