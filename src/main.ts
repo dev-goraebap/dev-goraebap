224627974cec4b45d5148b56d2747ad0fd53ea0e
dev-goraebap <dev.goraebap@gmail.com>
2025-08-04 02:44:53 +0900

    feat: 업로드 관련 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
+import * as express from 'express';
 import { join } from 'path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use(cookieParser());
+
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
   app.use(
     session({
