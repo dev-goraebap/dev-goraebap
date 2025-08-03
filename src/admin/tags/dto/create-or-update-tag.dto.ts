ba0e7768a70369917e367c86ce774a3dcdf95112
dev-goraebap <dev.goraebap@gmail.com>
2025-08-03 18:40:24 +0900

    refactor: 추가, 변경 공용 dto로 개선

---
+++ src/admin/tags/dto/create-or-update-tag.dto.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const CreateOrUpdateTagSchema = z
+  .object({
+    name: z.string().min(1, '이름을 입력해주세요.'),
+    description: z.string().optional(),
+  })
+
+export type CreateOrUpdateTagDto = z.infer<typeof CreateOrUpdateTagSchema>;
--- src/admin/tags/dto/create-tag.dto.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const CreateTagSchema = z
-  .object({
-    name: z.string().min(1, '이름을 입력해주세요.'),
-    description: z.string().min(1, '내용을 입력해주세요.'),
-  })
-
-export type CreateTagDto = z.infer<typeof CreateTagSchema>;
