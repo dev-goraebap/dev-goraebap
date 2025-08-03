11d471ed7a0a0db86f3070db7461085db3fe457e
dev-goraebap <dev.goraebap@gmail.com>
2025-08-03 18:40:04 +0900

    refactor: zod 유효성 검사 파이프 개선

--- src/common/pipes/zod.pipe.ts
+++ src/common/pipes/zod.pipe.ts
@@ -11,22 +11,21 @@ export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: z.ZodType<any, any>) {}
 
   transform(value: any, metadata: ArgumentMetadata) {
-    if (metadata.type !== 'body') {
+    if (metadata.type !== 'body' && metadata.type !== 'query') {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return value;
     }
 
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      const objectToValidate = value?.document || value; // 요청 본문 구조에 따라 조정
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-      const parsedValue = this.schema.parse(objectToValidate);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const parsedValue = this.schema.parse(value);
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return parsedValue;
     } catch (error) {
       if (error instanceof ZodError) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-        const errorMessages = error.issues.map((issue) => `${issue.path.join('.')}: ${issue.message}`).join(', ');
+        const errorMessages = error.issues
+          .map((issue) => `${issue.path.join('.')}: ${issue.message}`)
+          .join(', ');
         throw new BadRequestException(errorMessages);
       }
       throw new BadRequestException(
