5415b9964aefa29484c2e4164c3f64eb34ebdecd
dev-goraebap <dev.goraebap@gmail.com>
2025-08-05 03:01:06 +0900

    refactor: 파일 인터셉터 불필요해짐

--- src/app/admin/posts/posts.controller.ts
+++ src/app/admin/posts/posts.controller.ts
@@ -4,11 +4,8 @@ import {
   Get,
   Post,
   Req,
-  Res,
-  UploadedFile,
-  UseInterceptors,
+  Res
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
 import { NestMvcReq } from 'nestjs-mvc-tools';
 
@@ -25,14 +22,11 @@ export class PostsController {
   }
 
   @Post()
-  @UseInterceptors(FileInterceptor('image'))
   create(
     @Req() req: NestMvcReq,
-    @UploadedFile() imageFile: Express.Multer.File,
     @Body() dto: any,
     @Res() res: Response,
   ) {
-    console.log(imageFile);
     console.log(dto);
     req.flash.success('게시물 저장 완료');
     return res.redirect('/admin/posts');
