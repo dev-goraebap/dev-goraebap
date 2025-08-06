bfa531689b76502d55c7729e98e143a4849363f2
dev-goraebap <dev.goraebap@gmail.com>
2025-08-06 19:01:10 +0900

    feat: add image loader

--- resources/src/controllers/index.js
+++ resources/src/controllers/index.js
@@ -1,6 +1,8 @@
 import { application } from './application';
 import { EditorController } from './editor.controller';
 import { FileUploadController } from './file-upload.controller';
+import { ImageLoaderController } from './image-loader.controller';
 
 application.register('file-upload', FileUploadController);
 application.register('editor', EditorController);
+application.register('image-loader', ImageLoaderController);
