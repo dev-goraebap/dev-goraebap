250cda613553e11c2d6f8e100a6e3305a9899b7e
dev-goraebap <dev.goraebap@gmail.com>
2025-08-07 16:21:01 +0900

    refactor

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,25 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { NestMvcModule } from 'nestjs-mvc-tools';
-import { join } from 'path';
 
 import { AppModule } from './app/app.module';
-import { currentThemeHelper, isCurrentRouteHelper, queryHelper, TypeOrmOptionsImpl } from './config';
+import { ConfigModule } from './config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: join(process.cwd(), `.env.${process.env.NODE_ENV}.local`),
-    }),
-    TypeOrmModule.forRootAsync({ useClass: TypeOrmOptionsImpl }),
-    NestMvcModule.forRoot({
-      view: {
-        helpers: [isCurrentRouteHelper, currentThemeHelper, queryHelper],
-      },
-      debug: true,
-    }),
+    ConfigModule,
     AppModule
   ],
 })
