171ee2457ad66ab8ad1ced001079f4397ea08764
dev-goraebap <dev.goraebap@gmail.com>
2025-08-07 16:20:33 +0900

    refactor: typeorm feature를 config 모듈에서 글로벌로 설정. 해당 모듈에서 필요 없어짐

--- src/app/admin/file-upload/file-upload.module.ts
+++ src/app/admin/file-upload/file-upload.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { BlobEntity } from 'src/shared/entities/blob.entity';
 import { FileUploadApiController } from './file-upload.api.controller';
 import { FileCleanupService } from './services/file-cleanup.service';
 import { FileUploadService } from './services/file-upload.service';
@@ -11,7 +9,6 @@ import { R2Service } from './services/r2.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([BlobEntity]),
     ScheduleModule.forRoot(),
   ],
   controllers: [
