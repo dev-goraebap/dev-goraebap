c9156c990b6d04354afb8acc1921940892ddf6ec
dev-goraebap <dev.goraebap@gmail.com>
2025-08-17 11:02:01 +0900

    update

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,24 +1,50 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { InjectRepository } from "@nestjs/typeorm";
-import { UserEntity } from "src/shared";
-import { Repository } from "typeorm";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  AttachmentQueryHelper,
+  PostEntity,
+  SeriesEntity,
+  UserEntity,
+} from 'src/shared';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService implements OnModuleInit {
-
   constructor(
     private readonly configService: ConfigService,
     @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(PostEntity)
+    private readonly postRepository: Repository<PostEntity>,
+    @InjectRepository(SeriesEntity)
+    private readonly seriesRepository: Repository<SeriesEntity>,
   ) {}
+
+  async getSeries() {
+    const qb = this.seriesRepository.createQueryBuilder('series');
+    AttachmentQueryHelper.withAttachments(qb, 'series');
+    qb.orderBy(`series.createdAt`, 'DESC');
+    qb.take(3);
+    return await qb.getMany();
+  }
+
+  async getPosts() {
+    const qb = this.postRepository
+      .createQueryBuilder('post')
+      .leftJoinAndSelect('post.tags', 'tag');
+    AttachmentQueryHelper.withAttachments(qb, 'post');
+    qb.orderBy(`post.createdAt`, 'DESC');
+    qb.take(6);
+    return await qb.getMany();
+  }
 
   async onModuleInit() {
     const username = this.configService.get('ADMIN_USERNAME');
     const hasUser = await this.userRepository.exists({
       where: {
-        email: username
-      }
+        email: username,
+      },
     });
     if (hasUser) {
       return;
@@ -27,7 +53,7 @@ export class AppService implements OnModuleInit {
     const newUser = this.userRepository.create({
       email: username,
       nickname: 'dev.goraebap',
-    })
+    });
     await this.userRepository.save(newUser);
   }
 }
