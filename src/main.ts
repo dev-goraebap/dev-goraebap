795e810d1a1f2a3f564b37d29144b7d33788faf5
dev-goraebap <dev.goraebap@gmail.com>
2025-08-17 11:30:11 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
@@ -15,16 +16,19 @@ async function bootstrap() {
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+  const configService = app.get(ConfigService);
+
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || 'your-secret-key',
+      name: 'connect.sid',
+      secret: configService.get('SESSION_SECRET')!,
       resave: false,
       saveUninitialized: false,
       cookie: {
-        maxAge: 24 * 60 * 60 * 1000, // 24 hours
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Send only over HTTPS
-        sameSite: 'lax',
+        maxAge: 24 * 60 * 60 * 1000, // 24시간 (밀리초)
+        httpOnly: true, // XSS 보안
+        secure: 'auto',
+        sameSite: 'lax', // CSRF 보호
       },
     }),
   );
