d0c05cd253b21ce4a8d6c4ece12762e4199c758c
dev-goraebap <dev.goraebap@gmail.com>
2025-09-20 13:18:39 +0900

    update

--- src/main.module.ts
+++ src/main.module.ts
@@ -3,13 +3,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config';
 import { PresentationModule } from './core/presentation/presentation.module';
 import { LoggerModule } from './shared';
+import { DrizzleModule } from './shared/drizzle';
 
 @Module({
   imports: [
     // prettier-ignore
     LoggerModule,
     ConfigModule,
-    PresentationModule
+    PresentationModule,
+    DrizzleModule
   ],
 })
 export class MainModule {}
