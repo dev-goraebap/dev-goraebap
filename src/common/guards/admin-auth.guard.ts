ff1b2e47ffe9186ff1cb1408dfab6d566128f1e2
dev-goraebap <dev.goraebap@gmail.com>
2025-10-06 13:16:39 +0900

    update

--- src/common/guards/admin-auth.guard.ts
+++ src/common/guards/admin-auth.guard.ts
@@ -5,19 +5,20 @@ import { UserEntity } from 'src/domain/user';
 export class AdminAuthGuard implements CanActivate {
   private readonly logger = new Logger(AdminAuthGuard.name);
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const req = context.switchToHttp().getRequest();
     const session = req.session;
 
-    // if (!session?.isAuthenticated) {
-    //   return false;
-    // }
-    // const user = await UserEntity.findByEmail(session?.userEmail);
-    // if (!user) {
-    //   this.logger.warn('사용자를 찾을 수 없습니다.');
-    //   return false;
-    // }
-    const user = UserEntity.getTestAdminUser();
+    if (!session?.isAuthenticated) {
+      return false;
+    }
+
+    const user = await UserEntity.findByEmail(session?.userEmail);
+    if (!user) {
+      this.logger.warn('사용자를 찾을 수 없습니다.');
+      return false;
+    }
+    
     req['currentUser'] = user;
     return true;
   }
